2f5b95279c43d9a7f1dabf3d42d548c7e25cd414
ren-wei <demonre@foxmail.com>
2021-08-10 22:47:09 +0800

    perf: 将数据源的格式由html改为markdown

--- scripts/updateAssets.js
+++ scripts/updateAssets.js
@@ -8,9 +8,8 @@
 const baseUrl = 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/zh/documents/option-parts/';
 const token = 'cd773ec255';
 
-const optionOutlineFileName = 'option-outline.js';
-
 const axios = require('axios');
+const html2markdown = require('html2markdown');
 const fs = require('fs');
 const path = require('path');
 
@@ -18,17 +17,32 @@ const path = require('path');
 async function getData(key) {
     await axios.get(`${baseUrl}option.${key}.js?${token}`).then(res => {
         if (res.status === 200) {
-            fs.writeFile(path.resolve(__dirname, `../assets/${key}.json`), res.data.slice(26 + key.length), () => {
+            const datas = JSON.parse(res.data.slice(26 + key.length));
+            const result = {};
+            Object.keys(datas).forEach(name => {
+                const item = datas[name];
+                try {
+                    item.desc = html2markdown(item.desc?.replace(/<iframe(([\s\S])*?)<\/iframe>/ig, ''));
+                } catch(e) {
+                    console.warn(key, '\t' ,name);
+                }
+                result[name] = item;
+            });
+            fs.writeFile(path.resolve(__dirname, `../assets/${key}.json`), JSON.stringify(result, null, 4), () => {
                 console.log(`${key}.json saved successfully.`);
             });
         }
     }).catch(e => {
-        console.log(key ,e.response.status);
+        if (e && e.response) {
+            console.warn(key ,e.response?.status);
+        } else {
+            console.warn(key);
+        }
     });
 }
 
 // 获取所有选项的类型信息
-axios.get(baseUrl + optionOutlineFileName + '?' + token).then(async res => {
+axios.get(baseUrl + 'option-outline.js?' + token).then(async res => {
     if (res.status === 200) {
         // 解析类型信息的数据并保存
         const typeMsgList = JSON.parse(res.data.slice(33)).children;
@@ -38,7 +52,14 @@ axios.get(baseUrl + optionOutlineFileName + '?' + token).then(async res => {
         // 获取顶级选项的数据
         const r = await axios.get(baseUrl + 'option.js?' + token);
         if (r.status === 200) {
-            fs.writeFile(path.resolve(__dirname, '../assets/option.json'), r.data.slice(25), () => {
+            const datas = JSON.parse(r.data.slice(25));
+            const result = {};
+            Object.keys(datas).forEach(key => {
+                const item = datas[key];
+                item.desc = html2markdown(item.desc.replace(/<iframe(([\s\S])*?)<\/iframe>/ig, ''));
+                result[key] = item;
+            });
+            fs.writeFile(path.resolve(__dirname, '../assets/option.json'), JSON.stringify(result, null, 4), () => {
                 console.log('option.json saved successfully.');
             });
             const options = Object.keys(JSON.parse(r.data.slice(25)));
