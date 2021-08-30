85d4aafd86e324f38295ce3e04cb8d3976a0465c
ren-wei <demonre@foxmail.com>
2021-08-30 23:34:44 +0800

    test: 增加测试说明和部分测试文件

---
+++ src/test/examples/00.ts
@@ -0,0 +1,6 @@
+// @ts-nocheck
+/** @type EChartsOption */
+const options = {};
+
+// 操作1：在位置 (3, 18) 按下 `Enter` 键
+// 预期1：与 html 文件对应的预期相同
---
+++ src/test/examples/01.ts
@@ -0,0 +1,11 @@
+// @ts-nocheck
+/** @type EChartsOption */
+const options = {
+    title: {},
+};
+
+// 操作1：在位置 (4, 15) 按下 `Enter` 键
+// 预期1：与 html 文件对应的预期相同
+//
+// 操作2：在位置 (4, 13) 按下 `Enter` 键
+// 预期2：与 html 文件对应的预期相同
