69ef357ee494ebf421a56e9bf1fb76149414e76b
ren-wei <demonre@foxmail.com>
2021-09-01 07:23:24 +0800

    test: 补充vue文件的测试

---
+++ src/test/examples/00.vue
@@ -0,0 +1,17 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+    computed: {
+        options() {
+            /** @type EChartsOption */
+            return {};
+            // 操作1：在位置 (11, 21) 按下 `Enter` 键
+            // 预期1：与 html 文件对应的预期相同
+        }
+    }
+});
+</script>
---
+++ src/test/examples/01.vue
@@ -0,0 +1,22 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+    computed: {
+        options() {
+            /** @type EChartsOption */
+            return {
+                title: {},
+            };
+            // 操作1：在位置 (12, 27) 按下 `Enter` 键
+            // 预期1：与 html 文件对应的预期相同
+            //
+            // 操作2：在位置 (12, 25) 按下 `Enter` 键
+            // 预期2：与 html 文件对应的预期相同
+        }
+    }
+});
+</script>
