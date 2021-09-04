5866bbb2f994d2ac21ea8d69aaf62db6c50d4119
ren-wei <demonre@foxmail.com>
2021-09-04 16:20:58 +0800

    test: 支持多个配置项对象的测试

---
+++ src/test/examples/04.vue
@@ -0,0 +1,20 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+    computed: {
+        options1() {
+            /** @type EChartsOption */
+            return {};
+        },
+        options2() {
+            /** @type EChartsOption */
+            return {};
+        }
+        // 操作和预期中位置改为 (11, 21) 和 (15, 21) 其他与 html 相同
+    }
+});
+</script>
