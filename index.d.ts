28262440135685c43b6bb9cd7406a98d5852e775
ren-wei <demonre@foxmail.com>
2021-09-21 17:36:13 +0800

    perf: 调整类型定义文件的格式

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
 interface AstNode {
-	type: string;
-	name: string;
-	start: number;
-	end: number;
-	properties: [AstNode];
-	elements: [AstNode];
-	key: AstNode;
-	value: AstNode;
-	raw: string;
+    type: string;
+    name: string;
+    start: number;
+    end: number;
+    properties: [AstNode];
+    elements: [AstNode];
+    key: AstNode;
+    value: AstNode;
+    raw: string;
 }
 
 type Key = 'type' | 'start' | 'end' | 'properties' | 'elements' | 'key' | 'value';
@@ -15,57 +15,56 @@ type Key = 'type' | 'start' | 'end' | 'properties' | 'elements' | 'key' | 'value
 type Paths = Array<string | SimpleObject>;
 
 interface TypeMsg {
-	type: string;
-	isObject: boolean;
-	isArray: boolean;
-	prop: string;
-	default: string;
-	children?: TypeMsg[];
+    type: string;
+    isObject: boolean;
+    isArray: boolean;
+    prop: string;
+    default: string;
+    children?: TypeMsg[];
 }
 
 interface BaseOptions {
-	code: string;
-	name: string;
-	title: string;
+    code: string;
+    name: string;
+    title: string;
 }
 
 interface RequiredRule {
-	key: string;
-	value: string;
-	valueRegExp: string; 
+    key: string;
+    value: string;
+    valueRegExp: string; 
 }
 
 interface UiControl {
-	type: string | string[];
-	default?: string
-	options?: string;
-	min?: string;
-	max?: string;
-	step?: string;
-	dims?: string;
-	required?: Array<RequiredRule>;
-	detailFileName?: string;
+    type: string | string[];
+    default?: string;
+    options?: string;
+    min?: string;
+    max?: string;
+    step?: string;
+    dims?: string;
+    required?: Array<RequiredRule>;
+    detailFileName?: string;
 }
 
 interface DescMsg {
-	desc: string;
-	exampleBaseOptions?: BaseOptions[];
-	uiControl?: UiControl;
+    desc: string;
+    uiControl?: UiControl;
 }
 
 interface DescMsgObject {
-	[propName: string]: DescMsg;
+    [propName: string]: DescMsg;
 }
 
 interface PathMsg {
-	name: string;
-	target: AstNode
+    name: string;
+    target: AstNode
 }
 
 interface KeyFunc {
-	[propName: string]: Function;
+    [propName: string]: Function;
 }
 
 interface SimpleObject {
-	[propName: string]: string | number | boolean;
+    [propName: string]: string | number | boolean;
 }
