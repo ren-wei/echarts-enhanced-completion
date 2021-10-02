25376b2012ee445916e54209e2f073a2e86f5048
ren-wei <demonre@foxmail.com>
2021-10-02 18:50:41 +0800

    test: 补全series测试

--- src/test/suite/series.test.ts
+++ src/test/suite/series.test.ts
@@ -1,13 +1,15 @@
 import * as vscode from 'vscode';
 import * as assert from 'assert';
-import * as path from 'path';
 
 import { provideCompletionItems } from '../../extension';
+import { getFileData, inputText } from './utils';
 
 suite('Extension Completion Series Option Test Suite', async() => {
     let document: vscode.TextDocument;
     let textEditor: vscode.TextEditor;
     let position: vscode.Position;
+
+    const seriesDescMsg = getFileData('series');
 
     async function init() {
         document = await vscode.workspace.openTextDocument({
@@ -22,15 +24,121 @@ suite('Extension Completion Series Option Test Suite', async() => {
         await init();
     });
 
-    test('顶级选项中series选项应该补全空数组');
+    test('顶级选项中series选项应该补全空数组', async() => {
+        position = await inputText(['\n', ''], textEditor, position);
+        const result = await provideCompletionItems(document, position) as vscode.CompletionItem[];
+        const target = result.find(v => (v.label as vscode.CompletionItemLabel).label === 'series');
+        assert.ok(target);
+        assert.strictEqual((target.insertText as vscode.SnippetString).value, 'series: [$0],');
+    });
 
-    test('空数组中应该返回所有series选项');
+    test('空数组中应该返回所有series选项', async() => {
+        position = await inputText([[
+            '',
+            '    series: [',
+            '        ',
+        ].join('\n'), [
+            '',
+            '    ]',
+        ].join('\n')], textEditor, position);
+        const result = await provideCompletionItems(document, position) as vscode.CompletionItem[];
+        assert.strictEqual(result.length, Object.keys(seriesDescMsg).length);
+        Object.entries(seriesDescMsg).forEach(([key, descMsg]) => {
+            const target = result.find(v => (v.label as vscode.CompletionItemLabel).label === key);
+            assert.ok(target);
+            assert.strictEqual((target.documentation as vscode.MarkdownString).value, descMsg.desc);
+        });
+    });
 
-    test('选项值应该是一个包含了必须属性和值的对象');
+    test('选项值应该是一个包含了必须属性和值的对象', async() => {
+        position = await inputText([[
+            '',
+            '    series: [',
+            '        ',
+        ].join('\n'), [
+            '',
+            '    ]',
+        ].join('\n')], textEditor, position);
+        const result = await provideCompletionItems(document, position) as vscode.CompletionItem[];
+        assert.strictEqual(result.length, Object.keys(seriesDescMsg).length);
+        Object.entries(seriesDescMsg).forEach(([key, descMsg]) => {
+            const target = result.find(v => (v.label as vscode.CompletionItemLabel).label === key);
+            assert.ok(target);
+            assert.strictEqual((target.insertText as vscode.SnippetString).value, `{\n\ttype: ${(descMsg.uiControl as UiControl).required![0].value},\n},`);
+        });
+    });
 
-    test("type为'line'的对象中应该返回 series-line 中所有的直接子级选项");
+    test("type为'line'的对象中应该返回 series-line 中所有的直接子级选项", async() => {
+        position = await inputText([[
+            '',
+            '    series: [',
+            '        {',
+            '            type: "line",',
+            '            ',
+        ].join('\n'), [
+            '',
+            '        }',
+            '    ]',
+        ].join('\n')], textEditor, position);
+        const result = await provideCompletionItems(document, position) as vscode.CompletionItem[];
+        const seriesLineDescMsg: DescMsgObject = {};
+        Object.entries(getFileData('series-line')).forEach(([key, item]) => {
+            if (!key.includes('.')) {
+                seriesLineDescMsg[key] = item;
+            }
+        });
+        assert.strictEqual(result.length, Object.keys(seriesLineDescMsg).length);
+        Object.entries(seriesLineDescMsg).forEach(([key, descMsg]) => {
+            const target = result.find(v => (v.label as vscode.CompletionItemLabel).label === key);
+            assert.ok(target);
+            assert.strictEqual((target.documentation as vscode.MarkdownString).value, descMsg.desc);
+        });
+    });
 
-    test('数组中已经存在的值不应该被过滤');
+    test('数组中已经存在的值不应该被过滤', async() => {
+        position = await inputText([[
+            '',
+            '    series: [',
+            '        {',
+            '            type: "line",',
+            '        },',
+            '        ',
+        ].join('\n'), [
+            '',
+            '    ]',
+        ].join('\n')], textEditor, position);
+        const result = await provideCompletionItems(document, position) as vscode.CompletionItem[];
+        assert.strictEqual(result.length, Object.keys(seriesDescMsg).length);
+        Object.entries(seriesDescMsg).forEach(([key, descMsg]) => {
+            const target = result.find(v => (v.label as vscode.CompletionItemLabel).label === key);
+            assert.ok(target);
+            assert.strictEqual((target.documentation as vscode.MarkdownString).value, descMsg.desc);
+            assert.strictEqual((target.insertText as vscode.SnippetString).value, `{\n\ttype: ${(descMsg.uiControl as UiControl).required![0].value},\n},`);
+        });
+    });
 
-    test('对象格式应该触发对应的补全');
+    test('对象格式应该触发对应的补全', async() => {
+        position = await inputText([[
+            '',
+            '    series: {',
+            '        type: "line",',
+            '        ',
+        ].join('\n'), [
+            '',
+            '    }',
+        ].join('\n')], textEditor, position);
+        const result = await provideCompletionItems(document, position) as vscode.CompletionItem[];
+        const seriesLineDescMsg: DescMsgObject = {};
+        Object.entries(getFileData('series-line')).forEach(([key, item]) => {
+            if (!key.includes('.')) {
+                seriesLineDescMsg[key] = item;
+            }
+        });
+        assert.strictEqual(result.length, Object.keys(seriesLineDescMsg).length);
+        Object.entries(seriesLineDescMsg).forEach(([key, descMsg]) => {
+            const target = result.find(v => (v.label as vscode.CompletionItemLabel).label === key);
+            assert.ok(target);
+            assert.strictEqual((target.documentation as vscode.MarkdownString).value, descMsg.desc);
+        });
+    });
 });
