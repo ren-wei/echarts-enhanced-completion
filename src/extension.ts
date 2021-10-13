7c586f1dda7e5c7d00dcbcbd124d07a63c822dfd
ren-wei <demonre@foxmail.com>
2021-10-13 16:14:47 +0800

    perf: 增加代码检查示例

--- src/extension.ts
+++ src/extension.ts
@@ -22,6 +22,24 @@ export function provideHover(document: vscode.TextDocument, position: vscode.Pos
     return options.getHover();
 }
 
+export function updateDiagnostics(document: vscode.TextDocument, collection: vscode.DiagnosticCollection): void {
+    // TODO: 下面是示例，需要更改
+    if (document) {
+        collection.set(document.uri, [{
+            code: 'xAxis',
+            message: 'cannot assign twice to immutable variable `x`',
+            range: new vscode.Range(new vscode.Position(3, 5), new vscode.Position(3, 9)),
+            severity: vscode.DiagnosticSeverity.Warning,
+            source: 'echarts-enhanced-completion',
+            // relatedInformation: [
+            //     new vscode.DiagnosticRelatedInformation(new vscode.Location(document.uri, new vscode.Range(new vscode.Position(1, 8), new vscode.Position(1, 9))), 'first assignment to `x`'),
+            // ],
+        }]);
+    } else {
+        collection.clear();
+    }
+}
+
 export function start() {
     store = new Store('echarts-option', Config.lang);
 }
@@ -35,6 +53,18 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(vscode.languages.registerHoverProvider(['html', 'javascript', 'typescript', 'vue'], {
         provideHover,
+    }));
+
+    const collection = vscode.languages.createDiagnosticCollection('echarts-options-diagnostic');
+    if (vscode.window.activeTextEditor) {
+        updateDiagnostics(vscode.window.activeTextEditor.document, collection);
+    }
+
+    context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(textDocumentChangeEvent => {
+        // TODO: 需要更改限制条件
+        if (textDocumentChangeEvent.document) {
+            updateDiagnostics(textDocumentChangeEvent.document, collection);
+        }
     }));
 }
 
