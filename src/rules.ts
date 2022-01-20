1d04024f775c3f22a1f22445241e7d7936225a1d
ren-wei <demonre@foxmail.com>
2022-01-20 17:44:17 +0800

    feat: 从资源目录获取规则

--- src/rules.ts
+++ src/rules.ts
@@ -1,5 +1,22 @@
+import * as path from 'path';
+import * as fs from 'fs';
 import Config from './config';
 
+const cache = {
+    'en': new Map<string, DependRules>(),
+    'zh': new Map<string, DependRules>(),
+};
+
 export default function(key: string): DependRules {
-    return [];
+    if (cache[Config.language].has(key)) {
+        return cache[Config.language].get(key) as DependRules;
+    }
+    const fileName = path.resolve(__dirname + `../../assets/rules/${Config.language}/${key}.json`);
+    if (fs.existsSync(fileName)) {
+        const result = JSON.parse(fs.readFileSync(fileName, { encoding: 'utf8' }));
+        cache[Config.language].set(key, result);
+        return result;
+    } else {
+        return [];
+    }
 };
